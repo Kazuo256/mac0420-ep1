cc889f732bde0d417e5268bcee1efb065d6930c2
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-04-16 08:35:44 -0300

    ep1::Window documentation.

--- src/window.h
+++ src/window.h
@@ -7,16 +7,28 @@
 
 namespace ep1 {
 
+/** Represents a graphic window. */
 class Window {
   public:
+    /** Constructor.
+     ** @param caption - The window's caption. */
     explicit Window (const std::string& caption);
+    /** Destructor. */
     ~Window () {}
+    /** Initializes the window.
+     ** Even if created, a window is only displayed if it has been initialized
+     ** before. */
     void init ();
   private:
+    /** Glut window's id. */
     int id_;
+    /** Reference base for all created and initialized windows. */
     static std::map<int, Window*> windows_;
+    /** Display callback function for all windows. */
     static void display ();
+    /** Reshape callback function for all windows. */
     static void reshape (int w, int h);
+    /** Mouse callback function for all windows. */
     static void mouse (int btn, int state, int x, int y);
 };
 
