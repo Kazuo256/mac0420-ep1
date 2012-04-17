7a3ad3d9a82395544ddf9af80f2b36388ca7f523
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-04-17 11:25:21 -0300

    ep1::Vec3D, changed double to GLdouble.

--- src/vec3D.h
+++ src/vec3D.h
@@ -2,30 +2,31 @@
 #ifndef EP1_VEC3D_H_
 #define EP1_VEC3D_H_
 
+#include "getglut.h"
 #include <cstring>
 
 namespace ep1 {
 
 class Vec3D {
   public:
-    explicit Vec3D (double x, double y, double z) :
+    explicit Vec3D (GLdouble x, GLdouble y, GLdouble z) :
       x_(x), y_(x), z_(z) {}
-    Vec3D (const double val[3]) :
+    Vec3D (const GLdouble val[3]) :
       x_(val[0]), y_(val[1]), z_(val[2]) {}
-    double x() const { return x_; }
-    double y() const { return y_; }
-    double z() const { return z_; }
-    const double* val() const { return val_; }
-    void set_x(double x) { x_ = x; }
-    void set_y(double y) { y_ = y; }
-    void set_z(double z) { z_ = z; }
-    void set_val(const double val[3]) { memcpy(val_, val, 3*sizeof(double)); }
+    GLdouble x() const { return x_; }
+    GLdouble y() const { return y_; }
+    GLdouble z() const { return z_; }
+    const GLdouble* val() const { return val_; }
+    void set_x(GLdouble x) { x_ = x; }
+    void set_y(GLdouble y) { y_ = y; }
+    void set_z(GLdouble z) { z_ = z; }
+    void set_val(const GLdouble val[3]) { memcpy(val_, val, 3*sizeof(GLdouble)); }
   private:
     union {
       struct {
-        double x_, y_, z_;
+        GLdouble x_, y_, z_;
       };
-      double val_[3];
+      GLdouble val_[3];
     };
 };
 
