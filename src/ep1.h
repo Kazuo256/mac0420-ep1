fa2c5a06f99f8224f65318b584c9d2bc33e07815
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-04-19 17:29:36 -0300

    Documented ep1.h.

--- src/ep1.h
+++ src/ep1.h
@@ -4,8 +4,14 @@
 
 namespace ep1 {
 
+/// Initializes the program.
+/** @param argc Frist parameter from main.
+ ** @param argv Second parameter from main.
+ ** First arguments are for GLUT. The first non-GLUT argument should be the name
+ ** of the input file from where the force field will be read. */
 void init (int argc, char **argv);
 
+/// Runs the program.
 void run ();
 
 } // namespace ep1
